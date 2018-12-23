a61247b05c3eca43fc688bc9e0883838277a115c
annacho <annacho13@gmail.com>
2018-12-23 10:51:12 -0800

    Add redux-thunk

--- src/containers/Checkout/ContactData.js
+++ src/containers/Checkout/ContactData.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
-import axios from '../../../axios-orders';
+import axios from '../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
